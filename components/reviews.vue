ae8def64276a3eb26c90dcdbdf9b755e628da915
iasvobodin <svobodinaphoto@MacBook-Pro-Ivan.local>
2020-02-14 01:59:14 +0500

    отзывы

---
+++ components/reviews.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="review">
+    <div class="name__holder">
+      <h3 class="review__name">
+        {{ reviews[counter].name }}
+      </h3>
+    </div>
+    <div class="review__holder">
+      <p class="review__body">
+        {{ reviews[counter].body }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import reviews from '~/static/data/reviews.json'
+export default {
+  data () {
+    return {
+      reviews,
+      counter: 1
+    }
+  },
+  computed: {
+    randomReviews () {
+      const counter = Math.floor(this.reviews.length * Math.random())
+      return this.reviews[counter]
+    }
+  },
+  mounted () {
+    this.rReviews()
+  },
+  methods: {
+    rReviews () {
+      setInterval(() => {
+        this.counter = Math.floor(this.reviews.length * Math.random())
+      }, 12000)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+::-webkit-scrollbar {
+  width: 1px;
+  height: 1px;
+  &-button {
+    width: 1px;
+    height: 1px;
+  }
+}
+.review {
+  position: relative;
+  margin-top: 20vh;
+  height: 50vh;
+  width: 98%;
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+  &__name {
+    margin: 0;
+    font-size: 20vw;
+    line-height: 20vw;
+    font-weight: 700;
+    color: #03a9f4;
+    opacity: 0.2;
+  }
+  &__body {
+    font-weight: 200;
+    color: white;
+    margin: 0;
+    padding: 10px;
+    font-size: 26px;
+    white-space: pre-wrap;
+  }
+  &__holder {
+    border-color: #03a9f4;
+    /* border-style: solid; */
+    /* border-width: 1px; */
+    border-radius: 5px;
+    /* background-color: #fff; */
+    margin-left: 20%;
+    width: 80%;
+    height: 40vh;
+    overflow-y: auto;
+  }
+}
+.name__holder {
+  /* transform: rotate(-90deg);
+  transform-origin: left bottom; */
+  position: absolute;
+  width: 80vw;
+  overflow: hidden;
+  left: 2%;
+  top: -14vw;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,24 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        SvobodinaPhoto
-      </h1>
-      <h2 class="subtitle">
-        the best photographer ever
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <nuxt-link class="button--green" to="/photoseries">
-          Фотосерии
-        </nuxt-link>
-      </div>
-    </div>
+    <Reviews />
+    <nuxt-link class="button--green" to="/photoseries">
+      Фотосерии
+    </nuxt-link>
   </div>
 </template>
 
 <script>
 // import { TweenMax, Power2 } from 'gsap'
-import Logo from '~/components/Logo.vue'
 
 export default {
-  transition: {
-    name: 'bounce',
-    mode: 'in-out'
-  },
   components: {
-    Logo
+    Reviews: () => import('~/components/reviews.vue')
   }
 }
 </script>
 
 <style>
-.bounce-enter, .bounce-leave-to{
-  transform: translateY(-100%);
-}
-/* .bounce-enter-to, .bounce-leave{
-  transform: translateY(100%);
-} */
-.bounce-enter-active, .bounce-leave-active {
-  transition: all 1s ease;
-}
-
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+body{
+  background-color: black;
 }
 </style>
